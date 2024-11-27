a21a5b1b7a0a590748fdea1da51a309cec7d3f89
Sayoff <sayofflives@gmail.com>
2024-11-27 11:39:37 -0300

    THE ONLY ONE

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC6nBgz_E_TKVLfv1Q0pYXnY7CkelAl5XI",
+    authDomain: "deckgame-68b01.firebaseapp.com",
+    projectId: "deckgame-68b01",
+    storageBucket: "deckgame-68b01.firebasestorage.app",
+    messagingSenderId: "750154670385",
+    appId: "1:750154670385:web:0d65c173a4cf65c99f1010",
+    measurementId: "G-8G66VE7QZ6"
+  };
+firebase.initializeApp(firebaseConfig);
